6c87831c5c05aec631996d80d48884d1ee8757db
Pablo Hoffman <pablo@pablohoffman.com>
2011-04-14 11:10:00 -0300

    Fixed bug in scmongo.util getting the BOT_NAME, and rearranged code layout

--- scmongo/util.py
+++ scmongo/util.py
@@ -8,10 +8,16 @@ def get_database(settings):
     """
 
     host = settings['HTTPCACHE_MONGO_HOST'] \
-        or os.environ.get('MONGO_HOST', 'localhost')
-    port = int(settings['HTTPCACHE_MONGO_PORT'] \
-        or os.environ.get('MONGO_PORT', '27017'))
+        or settings['MONGO_HOST'] \
+        or os.environ.get('MONGO_HOST') \
+        or 'localhost'
+    port = settings.getint('HTTPCACHE_MONGO_PORT') \
+        or settings.getint('MONGO_PORT') \
+        or int(os.environ.get('MONGO_PORT', '27017')) \
+        or 27017
     db = settings['HTTPCACHE_MONGO_DATABASE'] \
-        or os.environ.get('MONGO_DATABASE', settings.get['BOT_NAME'])
+        or settings['MONGO_DATABASE'] \
+        or os.environ.get('MONGO_DATABASE') \
+        or settings['BOT_NAME']
 
     return pymongo.Connection(host, port)[db]
